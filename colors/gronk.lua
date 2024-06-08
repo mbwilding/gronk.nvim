2b3b5c74ec63c0ae0af47df603a20c9edb08812a
Matthew Wilding <mbwilding@gmail.com>
2024-06-08 16:48:51 +0800

    update

--- colors/gronk.lua
+++ colors/gronk.lua
@@ -33,41 +33,41 @@ local trans = M.options.transparent and M.base.none or M.base.bg
 -- Set the highlights
 M.highlights = {
 	-- Theme
-	Normal = { fg = M.base.fg, bg = trans },              -- Current window
-	NormalNC = { fg = M.base.fg, bg = trans },            -- Non-current window
-	ColorColumn = { fg = M.base.fg, bg = trans },         -- Columns set with `colorcolumn`
+	ColorColumn = { fg = M.base.fg, bg = trans }, -- Columns set with `colorcolumn`
+	Normal = { fg = M.base.fg, bg = trans }, -- Current window
 	NormalFloat = { fg = M.base.fg, bg = M.base.window_bg }, -- Normal text in floating windows
-	NormalSB = { fg = M.base.fg, bg = trans },            -- Normal text in sidebar
+	NormalNC = { fg = M.base.fg, bg = trans }, -- Non-current window
+	NormalSB = { fg = M.base.fg, bg = trans }, -- Normal text in sidebar
 
 	-- Gutter
-	LineNr = { fg = M.primary, bg = M.base.none },          -- Current line number
-	CursorLineNr = { fg = M.primary, bg = M.base.none },    -- Unknown
+	CursorLineNr = { fg = M.primary, bg = M.base.none }, -- Unknown
+	LineNr = { fg = M.primary, bg = M.base.none }, -- Current line number
 	LineNrAbove = { fg = M.code.redundant, bg = M.base.none }, -- Above current line numbers
 	LineNrBelow = { fg = M.code.redundant, bg = M.base.none }, -- Below current line numbers
 
 	-- Generic
-	MatchParen = { fg = M.base.bg, bg = M.base.blue_dark },        -- Matching pair highlight
-	Cursor = { fg = M.base.fg, bg = M.secondary },                 -- Character under the cursor
-	lCursor = { fg = M.base.fg, bg = M.secondary },                -- Character under the cursor when `language-mapping`
-	CursorIM = { fg = M.base.fg, bg = M.base.bg },                 -- Character under the cursor in IME mode
-	CursorLine = { fg = M.base.blue_dark, bg = M.base.none },      -- Screen line at the cursor
-	IncSearch = { fg = M.base.bg, bg = M.base.blue_dark },         -- Yank highlight
-	Conceal = { fg = M.code.redundant, bg = M.base.none },         -- Grayed out
-	EndOfBuffer = { fg = M.base.bg },                              -- End of buffer `~`
-	DiffAdd = { fg = M.base.green },                               -- Diff add
-	DiffChange = { fg = M.base.orange_dark },                      -- Diff change
-	DiffDelete = { fg = M.code.error },                            -- Diff delete
-	ErrorMsg = { fg = M.code.error },                              -- Error messages on the commandline
-	VertSplit = { fg = M.base.window_accent, bg = M.base.window_bg }, -- Vertical split border
-	WinSeparator = { fg = trans, bg = M.base.none },               -- Window border
-	WinBar = { fg = M.base.fg, bg = M.base.window_bg },
-	WinBarNC = { fg = M.base.fg, bg = M.base.window_bg },
+	Conceal = { fg = M.code.redundant, bg = M.base.none }, -- Grayed out
+	Cursor = { fg = M.base.fg, bg = M.secondary }, -- Character under the cursor
+	CursorIM = { fg = M.base.fg, bg = M.base.bg }, -- Character under the cursor in IME mode
+	CursorLine = { fg = M.base.blue_dark, bg = M.base.none }, -- Screen line at the cursor
+	DiffAdd = { fg = M.base.green }, -- Diff add
+	DiffChange = { fg = M.base.orange_dark }, -- Diff change
+	DiffDelete = { fg = M.code.error }, -- Diff delete
+	EndOfBuffer = { fg = M.base.bg }, -- End of buffer `~`
+	ErrorMsg = { fg = M.code.error }, -- Error messages on the commandline
+	Folded = { fg = M.base.blue_dark, bg = M.base.none }, -- Folded gutter
+	IncSearch = { fg = M.base.bg, bg = M.base.blue_dark }, -- Yank highlight
+	MatchParen = { fg = M.base.bg, bg = M.base.blue_dark }, -- Matching pair highlight
 	StatusLine = { fg = M.base.fg, bg = trans },
 	StatusLineNC = { fg = M.base.fg, bg = trans },
-	Folded = { fg = M.base.blue_dark, bg = M.base.none }, -- Folded gutter
 	TabLine = { fg = M.base.fg, bg = M.base.window_accent },
-	TabLineSel = { fg = M.base.window_bg, bg = M.base.blue_dark },
 	TabLineFill = { bg = M.base.bg },
+	TabLineSel = { fg = M.base.window_bg, bg = M.base.blue_dark },
+	VertSplit = { fg = M.base.window_accent, bg = M.base.window_bg }, -- Vertical split border
+	WinBar = { fg = M.base.fg, bg = M.base.window_bg },
+	WinBarNC = { fg = M.base.fg, bg = M.base.window_bg },
+	WinSeparator = { fg = trans, bg = M.base.none }, -- Window border
+	lCursor = { fg = M.base.fg, bg = M.secondary }, -- Character under the cursor when `language-mapping`
 
 	-- NeoVim
 	healthError = { fg = M.base.red },
@@ -83,43 +83,43 @@ M.highlights = {
 	CmpItemAbbrMatchFuzzy = { fg = M.primary, bg = M.base.none },
 
 	-- Lazy
+	LazyButton = { fg = M.base.fg, bg = M.base.window_accent },
+	LazyButtonActive = { fg = M.base.bg, bg = M.secondary },
 	LazyComment = { fg = M.code.comment },
+	LazyH1 = { fg = M.base.bg, bg = M.secondary },
 	LazyProp = { fg = M.secondary },
 	LazyReasonPlugin = { fg = M.code.comment },
 	LazySpecial = { fg = M.secondary },
-	LazyH1 = { fg = M.base.bg, bg = M.secondary },
-	LazyButton = { fg = M.base.fg, bg = M.base.window_accent },
-	LazyButtonActive = { fg = M.base.bg, bg = M.secondary },
 	Title = { fg = M.secondary },
 
 	-- Mason
 	MasonHeader = { fg = M.base.bg, bg = M.secondary },
 	MasonHighlight = { fg = M.base.blue_dark, bg = M.base.none },
+	MasonHighlightBlock = { fg = M.secondary, bg = M.base.window_accent },
 	MasonHighlightBlockBold = { fg = M.base.window_accent, bg = M.secondary },
 	MasonMuted = { fg = M.code.redundant, bg = M.base.none },
 	MasonMutedBlock = { fg = M.base.fg, bg = M.base.window_accent },
-	MasonHighlightBlock = { fg = M.secondary, bg = M.base.window_accent },
 
 	-- File System
 	Directory = { fg = M.primary }, -- Directory color
 	OilFile = { fg = M.secondary }, -- Oil file color
 
 	-- Telescope
-	TelescopeNormal = { fg = M.primary, bg = M.base.window_bg },
 	TelescopeBorder = { fg = M.secondary, bg = M.base.window_bg },
 	TelescopeMatching = { fg = M.secondary, bg = M.base.window_bg },
+	TelescopeNormal = { fg = M.primary, bg = M.base.window_bg },
 
 	-- Which Key
 	WhichKey = { fg = M.primary },
-	WhichKeyGroup = { fg = M.primary },
 	WhichKeyDesc = { fg = M.primary },
-	WhichKeySeparator = { fg = M.primary },
 	WhichKeyFloat = { bg = M.base.window_bg },
+	WhichKeyGroup = { fg = M.primary },
+	WhichKeySeparator = { fg = M.primary },
 	WhichKeyValue = { fg = M.primary },
 
 	-- Copilot
-	CopilotSuggestion = { fg = M.base.gray_dark },
 	CopilotAnnotation = { fg = M.base.gray_light },
+	CopilotSuggestion = { fg = M.base.gray_dark },
 
 	-- DAP
 	DapBreakpoint = { fg = M.base.red },
@@ -127,37 +127,6 @@ M.highlights = {
 	DapStopped = { fg = M.base.red },
 
 	-- Code
-	["@attribute"] = { fg = M.code.attribute },
-	["@boolean"] = { fg = M.code.keyword },
-	["@character"] = { fg = M.code.number },
-	["@constant"] = { fg = M.code.constant },
-	["@constant.macro"] = { fg = M.code.macro },
-	["@constructor"] = { fg = M.code.struct },
-	["@function.macro"] = { fg = M.code.macro },
-	["@lsp.mod.declaration"] = { fg = M.code.module },
-	["@lsp.mod.library"] = { fg = M.code.module },
-	["@lsp.mod.static"] = { fg = M.code.constant },
-	["@lsp.type.invalidEscapeSequence"] = { fg = M.code.error },
-	["@lsp.type.formatSpecifier"] = { fg = M.code.struct },
-	["@lsp.type.keyword"] = { fg = M.code.keyword },
-	["@lsp.typemod.decorator.attribute"] = { fg = M.code.attribute },
-	["@lsp.typemod.derive"] = { fg = M.code.interface },
-	["@lsp.typemod.enum"] = { fg = M.code.enum },
-	["@lsp.typemod.enumMember"] = { fg = M.code.constant },
-	["@lsp.typemod.function"] = { fg = M.code.method },
-	["@lsp.typemod.interface.library"] = { fg = M.code.interface },
-	["@lsp.typemod.macro"] = { fg = M.code.macro },
-	["@lsp.typemod.method"] = { fg = M.code.method },
-	["@lsp.typemod.operator.controlFlow"] = { fg = M.code.keyword },
-	["@lsp.typemod.property.declaration"] = { fg = M.code.member },
-	["@lsp.typemod.property"] = { fg = M.code.member },
-	["@lsp.typemod.struct"] = { fg = M.code.struct },
-	["@lsp.typemod.typeAlias.library"] = { fg = M.code.struct }, -- link = "Type"
-	["@lsp.typemod.variable"] = { fg = M.code.variable },
-	["@module"] = { fg = M.code.namespace },
-	["@string.escape"] = { fg = M.code.escape },
-	["@variable"] = { fg = M.code.variable },
-	["@variable.member"] = { fg = M.code.member },
 	Comment = { fg = M.code.comment },
 	Delimiter = { fg = M.code.delimiter },
 	Function = { fg = M.code.method },
@@ -168,26 +137,57 @@ M.highlights = {
 	Statement = { fg = M.code.keyword },
 	String = { fg = M.code.string },
 	Type = { fg = M.code.struct },
+	["@attribute"] = { fg = M.code.attribute },
+	["@boolean"] = { fg = M.code.keyword },
+	["@character"] = { fg = M.code.number },
+	["@constant"] = { fg = M.code.constant },
+	["@constant.macro"] = { fg = M.code.macro },
+	["@constructor"] = { fg = M.code.struct },
+	["@function.macro"] = { fg = M.code.macro },
+	["@lsp.mod.declaration"] = { fg = M.code.module },
+	["@lsp.mod.library"] = { fg = M.code.module },
+	["@lsp.mod.static"] = { fg = M.code.constant },
+	["@lsp.type.formatSpecifier"] = { fg = M.code.struct },
+	["@lsp.type.invalidEscapeSequence"] = { fg = M.code.error },
+	["@lsp.type.keyword"] = { fg = M.code.keyword },
+	["@lsp.typemod.decorator.attribute"] = { fg = M.code.attribute },
+	["@lsp.typemod.derive"] = { fg = M.code.interface },
+	["@lsp.typemod.enum"] = { fg = M.code.enum },
+	["@lsp.typemod.enumMember"] = { fg = M.code.constant },
+	["@lsp.typemod.function"] = { fg = M.code.method },
+	["@lsp.typemod.interface.library"] = { fg = M.code.interface },
+	["@lsp.typemod.macro"] = { fg = M.code.macro },
+	["@lsp.typemod.method"] = { fg = M.code.method },
+	["@lsp.typemod.operator.controlFlow"] = { fg = M.code.keyword },
+	["@lsp.typemod.property"] = { fg = M.code.member },
+	["@lsp.typemod.property.declaration"] = { fg = M.code.member },
+	["@lsp.typemod.struct"] = { fg = M.code.struct },
+	["@lsp.typemod.typeAlias.library"] = { fg = M.code.struct }, -- link = "Type"
+	["@lsp.typemod.variable"] = { fg = M.code.variable },
+	["@module"] = { fg = M.code.namespace },
+	["@string.escape"] = { fg = M.code.escape },
+	["@variable"] = { fg = M.code.variable },
+	["@variable.member"] = { fg = M.code.member },
 
 	-- Diagnostics
 	DiagnosticUnnecessary = { fg = M.code.redundant },
 
 	-- nvim-notify
+	NoiceAttr = { fg = M.base.fg },
 	NotifyBackground = { bg = M.base.bg },
 	NotifyINFOBody = { fg = M.base.fg },
-	NoiceAttr = { fg = M.base.fg },
 
 	-- Hints
 	LspInlayHint = { fg = M.base.gray_dark },
 	LspLens = { fg = M.base.gray_light },
 
 	-- Rust
-	["@type.qualifier.rust"] = { fg = M.code.keyword, underline = true },
-	["@lsp.mod.mutable"] = { underline = true },
 	["@lsp.mod.declaration.rust"] = { fg = M.base.fg },
-	["@lsp.typemod.namespace.crateRoot"] = { fg = M.code.namespace }, -- link = "@module"
-	["@lsp.typemod.interface.declaration"] = { fg = M.code.interface },
+	["@lsp.mod.mutable"] = { underline = true },
 	["@lsp.mod.unsafe"] = { fg = M.code.error },
+	["@lsp.typemod.interface.declaration"] = { fg = M.code.interface },
+	["@lsp.typemod.namespace.crateRoot"] = { fg = M.code.namespace }, -- link = "@module"
+	["@type.qualifier.rust"] = { fg = M.code.keyword, underline = true },
 
 	-- CSharp
 	["@lsp.type.stringEscapeCharacter.cs"] = { fg = M.code.escape },
@@ -203,16 +203,22 @@ vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSi
 vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
 vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
 vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
-vim.fn.sign_define('DapBreakpointCondition',
-	{ text = 'ﳁ', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
-vim.fn.sign_define('DapBreakpointRejected',
-	{ text = '', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
-vim.fn.sign_define('DapLogPoint', { text = '', texthl = 'DapLogPoint', linehl = 'DapLogPoint', numhl = 'DapLogPoint' })
-vim.fn.sign_define('DapStopped', { text = '', texthl = 'DapStopped', linehl = 'DapStopped', numhl = 'DapStopped' })
-vim.fn.sign_define('DapBreakpoint', {
-	text = '⦿',
-	texthl = 'DapBreakpoint',
-	linehl = 'DapBreakpoint',
-	numhl =
-	'DapBreakpoint'
+vim.fn.sign_define(
+	"DapBreakpointCondition",
+	{ text = "ﳁ", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+)
+vim.fn.sign_define(
+	"DapBreakpointRejected",
+	{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+)
+vim.fn.sign_define(
+	"DapLogPoint",
+	{ text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
+)
+vim.fn.sign_define("DapStopped", { text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
+vim.fn.sign_define("DapBreakpoint", {
+	text = "⦿",
+	texthl = "DapBreakpoint",
+	linehl = "DapBreakpoint",
+	numhl = "DapBreakpoint",
 })
