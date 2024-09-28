4776c436ead5ea09407ba44ae7b0aa23ffecf01f
Matthew Wilding <mbwilding@gmail.com>
2024-09-28 15:28:06 +0800

    update

--- lua/gronk/groups/base.lua
+++ lua/gronk/groups/base.lua
@@ -44,42 +44,8 @@ function M.get(c, opts)
         healthSuccess                              = { fg = c.method },
         healthWarning                              = { fg = c.module },
 
-        -- NeoGit
-        NeogitChangeAdded                          = { fg = c.method },
-        NeogitChangeDeleted                        = { fg = c.error },
-        NeogitChangeModified                       = { fg = c.module },
-        NeogitChangeRenamed                        = { fg = c.pink },
-        NeogitChangeUpdated                        = { fg = c.macro },
-        NeogitChangeCopied                         = { fg = c.keyword },
-        NeogitChangeNewFile                        = { fg = c.method },
-        NeogitChangeUnmerged                       = { fg = c.namespace },
-
         -- Diagnostics
         DiagnosticUnnecessary                      = { fg = c.redundant },
-
-        -- Cmp
-        CmpGhostText                               = { fg = c.redundant },
-        CmpDocumentation                           = { fg = c.fg, bg = c.window_bg },
-        CmpDocumentationBorder                     = { fg = c.window_accent, bg = c.window_bg },
-        CmpItemAbbr                                = { fg = c.fg, bg = c.none },
-        CmpItemAbbrDeprecated                      = { fg = c.redundant, bg = c.none, strikethrough = true },
-        CmpItemAbbrMatch                           = { fg = c.macro, bg = c.none },
-        CmpItemAbbrMatchFuzzy                      = { fg = c.keyword, bg = c.none },
-        CmpItemKindConstant                        = { fg = c.constant, bg = c.none },
-        CmpItemKindCopilot                         = { fg = c.namespace, bg = c.none },
-        CmpItemKindDefault                         = { fg = c.fg, bg = c.none },
-        CmpItemKindEnum                            = { fg = c.enum, bg = c.none },
-        CmpItemKindField                           = { fg = c.member, bg = c.none },
-        CmpItemKindFunction                        = { fg = c.method, bg = c.none },
-        CmpItemKindInterface                       = { fg = c.interface, bg = c.none },
-        CmpItemKindKeyword                         = { fg = c.keyword, bg = c.none },
-        CmpItemKindMethod                          = { fg = c.method, bg = c.none },
-        CmpItemKindModule                          = { fg = c.module, bg = c.none },
-        CmpItemKindReference                       = { fg = c.variable, bg = c.none },
-        CmpItemKindSnippet                         = { fg = c.macro, bg = c.none },
-        CmpItemKindStruct                          = { fg = c.struct, bg = c.none },
-        CmpItemKindText                            = { fg = c.fg, bg = c.none },
-        CmpItemKindVariable                        = { fg = c.variable, bg = c.none },
 
         -- Treesitter Context
         TreesitterContext                          = { bg = c.bg, link = c.none },
---
+++ lua/gronk/groups/neogit.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+M.url = "https://github.com/TimUntersberger/neogit"
+
+function M.get(c)
+    return {
+        -- NeogitBranch               = { fg = c.magenta },
+        -- NeogitRemote               = { fg = c.purple },
+        -- NeogitHunkHeader           = { bg = c.bg_highlight, fg = c.fg },
+        -- NeogitHunkHeaderHighlight  = { bg = c.fg_gutter, fg = c.blue },
+        -- NeogitDiffContextHighlight = { bg = Util.blend_bg(c.fg_gutter, 0.5), fg = c.fg_dark },
+        -- NeogitDiffDeleteHighlight  = { fg = c.git.delete, bg = c.diff.delete },
+        -- NeogitDiffAddHighlight     = { fg = c.git.add, bg = c.diff.add },
+
+        NeogitChangeAdded                          = { fg = c.method },
+        NeogitChangeDeleted                        = { fg = c.error },
+        NeogitChangeModified                       = { fg = c.module },
+        NeogitChangeRenamed                        = { fg = c.pink },
+        NeogitChangeUpdated                        = { fg = c.macro },
+        NeogitChangeCopied                         = { fg = c.keyword },
+        NeogitChangeNewFile                        = { fg = c.method },
+        NeogitChangeUnmerged                       = { fg = c.namespace },
+
+    }
+end
+
+return M
