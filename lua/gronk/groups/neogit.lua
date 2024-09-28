27829a440050c07a453a4ddb21c43ebbd151c74a
Matthew Wilding <mbwilding@gmail.com>
2024-09-29 02:32:18 +0800

    update

--- lua/gronk/groups/neogit.lua
+++ lua/gronk/groups/neogit.lua
@@ -2,13 +2,13 @@ local M = {}
 
 function M.get(c, opts)
     return {
-        -- NeogitBranch               = { fg = c.magenta },
-        -- NeogitRemote               = { fg = c.purple },
-        -- NeogitHunkHeader           = { bg = c.bg_highlight, fg = c.fg },
-        -- NeogitHunkHeaderHighlight  = { bg = c.fg_gutter, fg = c.blue },
-        -- NeogitDiffContextHighlight = { bg = Util.blend_bg(c.fg_gutter, 0.5), fg = c.fg_dark },
-        -- NeogitDiffDeleteHighlight  = { fg = c.git.delete, bg = c.diff.delete },
-        -- NeogitDiffAddHighlight     = { fg = c.git.add, bg = c.diff.add },
+        NeogitBranch               = { fg = c.macro },
+        NeogitRemote               = { fg = c.struct },
+        -- NeogitHunkHeader           = { bg = c.window_accent, fg = c.fg },
+        -- NeogitHunkHeaderHighlight  = { bg = c.redundant, fg = c.keyword },
+        -- NeogitDiffContextHighlight = { bg = c.bg, fg = c.fg },
+        NeogitDiffDeleteHighlight  = { fg = c.fg, bg = c.error },
+        NeogitDiffAddHighlight     = { fg = c.fg, bg = c.good },
 
         NeogitChangeAdded = { fg = c.good },
         NeogitChangeDeleted = { fg = c.error },
