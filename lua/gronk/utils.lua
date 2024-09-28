87fb0bcd7a2c45a4690247a32e5f2e92cbd68f5a
Matthew Wilding <mbwilding@gmail.com>
2024-09-28 13:49:28 +0800

    update

--- lua/gronk/utils.lua
+++ lua/gronk/utils.lua
@@ -4,6 +4,9 @@ M.none = "NONE"
 M.bg = "#000000"
 M.fg = "#ffffff"
 
+local me = debug.getinfo(1, "S").source:sub(2)
+me = vim.fn.fnamemodify(me, ":h:h")
+
 function M.mod(modname)
   if package.loaded[modname] then
     return package.loaded[modname]
