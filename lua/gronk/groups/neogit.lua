9f918334acab58be4dcc2d9a5f7c62069012a13f
Matthew Wilding <mbwilding@gmail.com>
2024-09-29 02:37:26 +0800

    update

--- lua/gronk/groups/neogit.lua
+++ lua/gronk/groups/neogit.lua
@@ -4,20 +4,20 @@ function M.get(c, opts)
     return {
         NeogitBranch               = { fg = c.macro },
         NeogitRemote               = { fg = c.struct },
-        -- NeogitHunkHeader           = { bg = c.window_accent, fg = c.fg },
-        -- NeogitHunkHeaderHighlight  = { bg = c.redundant, fg = c.keyword },
-        NeogitDiffContextHighlight = { bg = c.interface, fg = c.error },
+        -- NeogitHunkHeader           = { fg = c.fg, bg = c.window_accent },
+        -- NeogitHunkHeaderHighlight  = { fg = c.keyword, bg = c.redundant },
+        NeogitDiffContextHighlight = { fg = c.fg, bg = c.bg },
         NeogitDiffDeleteHighlight  = { fg = c.fg, bg = c.error },
         NeogitDiffAddHighlight     = { fg = c.fg, bg = c.good },
 
-        NeogitChangeAdded = { fg = c.good },
-        NeogitChangeDeleted = { fg = c.error },
-        NeogitChangeModified = { fg = c.warn },
-        NeogitChangeRenamed = { fg = c.warn },
-        NeogitChangeUpdated = { fg = c.warn },
-        NeogitChangeCopied = { fg = c.good },
-        NeogitChangeNewFile = { fg = c.good },
-        NeogitChangeUnmerged = { fg = c.redundant },
+        NeogitChangeAdded          = { fg = c.good },
+        NeogitChangeDeleted        = { fg = c.error },
+        NeogitChangeModified       = { fg = c.warn },
+        NeogitChangeRenamed        = { fg = c.warn },
+        NeogitChangeUpdated        = { fg = c.warn },
+        NeogitChangeCopied         = { fg = c.good },
+        NeogitChangeNewFile        = { fg = c.good },
+        NeogitChangeUnmerged       = { fg = c.redundant },
     }
 end
 
