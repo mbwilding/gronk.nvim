0a0e821343a001c812db4363730e5585d8019066
Matthew Wilding <mbwilding@gmail.com>
2024-09-29 02:54:40 +0800

    update

--- lua/gronk/groups/neogit.lua
+++ lua/gronk/groups/neogit.lua
@@ -2,22 +2,35 @@ local M = {}
 
 function M.get(c, opts)
     return {
-        NeogitBranch               = { fg = c.macro },
-        NeogitRemote               = { fg = c.struct },
         -- NeogitHunkHeader           = { fg = c.fg, bg = c.bg },
         -- NeogitHunkHeaderHighlight  = { fg = c.fg, bg = c.bg },
         -- NeogitDiffContextHighlight = { fg = c.fg, bg = c.bg },
-        NeogitDiffDeleteHighlight  = { fg = c.error, bg = c.bg },
-        NeogitDiffAddHighlight     = { fg = c.good, bg = c.bg },
+        -- NeogitDiffDeleteHighlight  = { fg = c.error, bg = c.bg },
+        -- NeogitDiffAddHighlight     = { fg = c.good, bg = c.bg },
 
-        NeogitChangeAdded          = { fg = c.good },
-        NeogitChangeDeleted        = { fg = c.error },
-        NeogitChangeModified       = { fg = c.warn },
-        NeogitChangeRenamed        = { fg = c.warn },
-        NeogitChangeUpdated        = { fg = c.warn },
-        NeogitChangeCopied         = { fg = c.good },
-        NeogitChangeNewFile        = { fg = c.good },
-        NeogitChangeUnmerged       = { fg = c.redundant },
+        NeogitBranch             = { fg = c.macro },
+        NeogitBranchHead         = { fg = c.interface },
+        NeogitRemote             = { fg = c.struct },
+        NeogitObjectId           = { fg = c.string },
+        NeogitHead               = { fg = c.keyword },
+        NeogitStash              = { fg = c.redundant },
+        NeogitFold               = { fg = c.redundant },
+        NeogitRebaseDone         = { fg = c.good },
+        NeogitTagName            = { fg = c.comment },
+        NeogitTagDistance        = { fg = c.number },
+        NeogitStatusHEAD         = { fg = c.fg },
+
+        NeogitSectionHeader      = { fg = c.module },
+        NeogitSectionHeaderCount = { fg = c.number },
+
+        NeogitChangeAdded        = { fg = c.good },
+        NeogitChangeDeleted      = { fg = c.error },
+        NeogitChangeModified     = { fg = c.warn },
+        NeogitChangeRenamed      = { fg = c.warn },
+        NeogitChangeUpdated      = { fg = c.warn },
+        NeogitChangeCopied       = { fg = c.good },
+        NeogitChangeNewFile      = { fg = c.good },
+        NeogitChangeUnmerged     = { fg = c.redundant },
     }
 end
 
