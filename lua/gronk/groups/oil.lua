68f21e5eb7c48656cc0283626494dd1754881dab
Matthew Wilding <mbwilding@gmail.com>
2024-09-29 19:41:40 +0800

    update

--- lua/gronk/groups/oil.lua
+++ lua/gronk/groups/oil.lua
@@ -3,6 +3,8 @@ local M = {}
 function M.get(c, opts)
     return {
         OilFile = { fg = c.macro },
+        OilLink = { fg = c.macro },
+        OilLinkTarget = { fg = c.comment },
     }
 end
 
