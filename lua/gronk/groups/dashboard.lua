b80c9134aa5835bc65728e746024003581777ad2
Matthew Wilding <mbwilding@gmail.com>
2024-10-03 18:02:03 +0800

    Create dashboard.lua

---
+++ lua/gronk/groups/dashboard.lua
@@ -0,0 +1,26 @@
+local M = {}
+
+function M.get(c, opts)
+    return {
+    -- General
+    DashboardHeader = { fg = c.keyword },
+    DashboardFooter = { fg = c.macro },
+
+    -- Hyper theme
+    DashboardProjectTitle = { fg = c.keyword },
+    DashboardProjectTitleIcon = { fg = c.keyword },
+    DashboardProjectIcon = { fg = c.keyword },
+    DashboardMruTitle = { fg = c.keyword },
+    DashboardMruIcon = { fg = c.keyword },
+    DashboardFiles = { fg = c.string },
+    DashboardShortCutIcon = { fg = c.macro },
+
+    -- Doom theme
+    DashboardDesc = { fg = c.string },
+    DashboardKey = { fg = c.keyword },
+    DashboardIcon = { fg = c.namespace },
+    DashboardShortCut = { fg = c.macro },
+    }
+end
+
+return M
