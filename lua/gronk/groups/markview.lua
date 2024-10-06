dbb15635a9efac63cbada7d6bfdb00b20f2437c4
Matthew Wilding <mbwilding@gmail.com>
2024-10-06 17:08:46 +0800

    2024-10-06-17-08-46

--- lua/gronk/groups/markview.lua
+++ lua/gronk/groups/markview.lua
@@ -8,17 +8,17 @@ function M.get(c, opts)
 
         -- Headings
         MarkviewHeading1Sign = { fg = c.attribute },
-        MarkviewHeading1 = { fg = c.struct, bg = c.attribute, bold = true },
+        MarkviewHeading1 = { fg = c.black, bg = c.attribute, bold = true },
         MarkviewHeading2Sign = { fg = c.namespace },
-        MarkviewHeading2 = { fg = c.module, bg = c.namespace, bold = true },
+        MarkviewHeading2 = { fg = c.black, bg = c.namespace, bold = true },
         MarkviewHeading3Sign = { fg = c.module },
         MarkviewHeading3 = { fg = c.black, bg = c.module, bold = true },
         MarkviewHeading4Sign = { fg = c.method },
-        MarkviewHeading4 = { fg = c.comment, bg = c.method, bold = true },
+        MarkviewHeading4 = { fg = c.black, bg = c.method, bold = true },
         MarkviewHeading5Sign = { fg = c.macro },
-        MarkviewHeading5 = { fg = c.keyword, bg = c.macro, bold = true },
+        MarkviewHeading5 = { fg = c.black, bg = c.macro, bold = true },
         MarkviewHeading6Sign = { fg = c.struct },
-        MarkviewHeading6 = { fg = c.attribute, bg = c.struct, bold = true },
+        MarkviewHeading6 = { fg = c.black, bg = c.struct, bold = true },
     }
 end
 
