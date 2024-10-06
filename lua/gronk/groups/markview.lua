1ef65b128b85e79c3d1c05894ec87de6ab107af9
Matthew Wilding <mbwilding@gmail.com>
2024-10-06 16:54:05 +0800

    2024-10-06-16-54-05

--- lua/gronk/groups/markview.lua
+++ lua/gronk/groups/markview.lua
@@ -7,18 +7,18 @@ function M.get(c, opts)
         MarkviewCodeInfo = { bg = c.window_bg },
 
         -- Headings
-        MarkviewHeading1Sign = { fg = c.fg, bg = c.attribute },
-        MarkviewHeading1 = { fg = c.fg, bg = c.attribute },
-        MarkviewHeading2Sign = { fg = c.fg, bg = c.namespace },
-        MarkviewHeading2 = { fg = c.fg, bg = c.namespace },
-        MarkviewHeading3Sign = { fg = c.fg, bg = c.module },
-        MarkviewHeading3 = { fg = c.fg, bg = c.module },
-        MarkviewHeading4Sign = { fg = c.fg, bg = c.method },
-        MarkviewHeading4 = { fg = c.fg, bg = c.method },
-        MarkviewHeading5Sign = { fg = c.fg, bg = c.macro },
-        MarkviewHeading5 = { fg = c.fg, bg = c.macro },
-        MarkviewHeading6Sign = { fg = c.fg, bg = c.struct },
-        MarkviewHeading6 = { fg = c.fg, bg = c.struct },
+        MarkviewHeading1Sign = { fg = c.bg, bg = c.attribute },
+        MarkviewHeading1 = { fg = c.bg, bg = c.attribute },
+        MarkviewHeading2Sign = { fg = c.bg, bg = c.namespace },
+        MarkviewHeading2 = { fg = c.bg, bg = c.namespace },
+        MarkviewHeading3Sign = { fg = c.bg, bg = c.module },
+        MarkviewHeading3 = { fg = c.bg, bg = c.module },
+        MarkviewHeading4Sign = { fg = c.bg, bg = c.method },
+        MarkviewHeading4 = { fg = c.bg, bg = c.method },
+        MarkviewHeading5Sign = { fg = c.bg, bg = c.macro },
+        MarkviewHeading5 = { fg = c.bg, bg = c.macro },
+        MarkviewHeading6Sign = { fg = c.bg, bg = c.struct },
+        MarkviewHeading6 = { fg = c.bg, bg = c.struct },
     }
 end
 
