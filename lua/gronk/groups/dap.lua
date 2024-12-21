138b344265f2c9bdcfe367ae00c46a6c0ec0650a
Matthew Wilding <mbwilding@gmail.com>
2024-12-22 03:14:13 +0800

    2024-12-22-03-14-13

--- lua/gronk/groups/dap.lua
+++ lua/gronk/groups/dap.lua
@@ -4,7 +4,7 @@ function M.get(c, opts)
     return {
         -- DapStopped = { fg = c.error },
 
-        DapBreakpoint = { fg = c.error },
+        DapBreakpoint = { bg = c.error },
         DapLogPoint = { fg = c.error },
         DapStoppedLine = { bg = c.error },
     }
