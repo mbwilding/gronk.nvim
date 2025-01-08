8f562225a079a4b93f33767af2aab1e9c76c04a7
Matthew Wilding <mbwilding@gmail.com>
2025-01-08 10:19:23 +0800

    2025-01-08-10-19-23

--- lua/gronk/groups/nvim-tree.lua
+++ lua/gronk/groups/nvim-tree.lua
@@ -2,10 +2,14 @@ local M = {}
 
 function M.get(c, opts)
     return {
-        NvimTreeDiagnosticErrorIcon = { fg = c.error },
-        NvimTreeDiagnosticWarnIcon = { fg = c.warn },
-        NvimTreeDiagnosticInfoIcon = { fg = c.info },
-        NvimTreeDiagnosticHintIcon = { fg = c.hint },
+        NvimTreeFileIcon = { fg = c.macro },
+        NvimTreeFolderIcon = { fg = c.namespace },
+        NvimTreeSymlinkIcon = { fg = c.comment },
+
+        -- NvimTreeDiagnosticErrorIcon = { fg = c.error },
+        -- NvimTreeDiagnosticWarnIcon = { fg = c.warn },
+        -- NvimTreeDiagnosticInfoIcon = { fg = c.info },
+        -- NvimTreeDiagnosticHintIcon = { fg = c.hint },
     }
 end
 
