17506047ef596b3d23fbb2dd5aeb624d27112208
Matthew Wilding <mbwilding@gmail.com>
2025-01-29 00:09:35 +0800

    2025-01-29-00-09-34

---
+++ lua/gronk/colors/mcdonalds.lua
@@ -0,0 +1,36 @@
+return {
+    -- Default
+    fg = "#e7b307",
+    bg = "#bf0f1e",
+    window_bg = "#dd1324",
+    window_accent = "#ee3a49",
+
+    -- Code
+    attribute = "#ed94c0",
+    comment = "#85c46c",
+    constant = "#83f1ff",
+    delimiter = "#a4a4a4",
+    enum = "#fbff91",
+    escape = "#ed94c0",
+    interface = "#9591ff",
+    keyword = "#6c95eb",
+    macro = "#4eade5",
+    member = "#91c2ff",
+    method = "#39cc8f",
+    module = "#ffc794",
+    namespace = "#ffb083",
+    number = "#ed94c0",
+    operator = "#eeeeee",
+    redundant = "#787878",
+    string = "#c9a26d",
+    struct = "#c191ff",
+    variable = "#edeecf",
+
+    -- Other
+    ok = "#39cc8f",
+    info = "#4eade5",
+    hint = "#66c3cc",
+    warn = "#ffb083",
+    error = "#ff4747",
+    inlay = "#4f5258",
+}
