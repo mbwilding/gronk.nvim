698a4a3df3840a99f7588185c643858865462bad
Matthew Wilding <mbwilding@gmail.com>
2025-01-29 00:44:48 +0800

    2025-01-29-00-44-48

--- lua/gronk/colors/mono.lua
+++ lua/gronk/colors/mono.lua
@@ -1,36 +1,36 @@
 return {
-    -- Default
+    -- default
     fg = "#bdbdbd",
     bg = "#000000",
     window_bg = "#101010",
     window_accent = "#202020",
 
-    -- Code
-    attribute = "#D3D3D3", -- Light gray
-    comment = "#404040",   -- Dark gray
-    constant = "#808080",  -- Gray
-    delimiter = "#696969", -- Dim gray
-    enum = "#BEBEBE",      -- Gainsboro
-    escape = "#B0C4DE",    -- Light Steel Blue (for subtle difference)
-    interface = "#DCDCDC", -- Gainsboro
-    keyword = "#A8A8A8",   -- Custom Dark gray
-    macro = "#C0C0C0",     -- Silver
-    member = "#B0B0B0",    -- Custom Light gray
-    method = "#909090",    -- Darker Gray
-    module = "#D8D8D8",    -- Light gray
-    namespace = "#C8C8C8", -- Custom Silver
-    number = "#AAAAAA",    -- Custom Medium gray
-    operator = "#E0E0E0",  -- Lighter Gray
-    redundant = "#787878", -- Iconic Silver
-    string = "#A7A7A7",    -- Between Dark and Medium gray
-    struct = "#CACACA",    -- Custom Light gray
-    variable = "#9E9E9E",  -- Medium-Dark Gray
+    -- code
+    attribute = "#d3d3d3",
+    comment = "#4d4d4d",
+    constant = "#808080",
+    delimiter = "#696969",
+    enum = "#bebebe",
+    escape = "#a8a8a8",
+    interface = "#dcdcdc",
+    keyword = "#8596aa",
+    macro = "#c0c0c0",
+    member = "#b0b0b0",
+    method = "#909090",
+    module = "#d8d8d8",
+    namespace = "#c8c8c8",
+    number = "#aaaaaa",
+    operator = "#e0e0e0",
+    redundant = "#787878",
+    string = "#a7a7a7",
+    struct = "#cacaca",
+    variable = "#9e9e9e",
 
-    -- Other
-    ok = "#B2B2B2",    -- Custom Medium-Light gray
-    info = "#BEBEBE",  -- Gainsboro
-    hint = "#CDCDCD",  -- Light gray
-    warn = "#8B8B8B",  -- Dark Gray
-    error = "#5C5C5C", -- Custom Dim gray
-    inlay = "#EAEAEA", -- Very Light gray
+    -- other
+    ok = "#b2b2b2",
+    info = "#bebebe",
+    hint = "#cdcdcd",
+    warn = "#8b8b8b",
+    error = "#5c5c5c",
+    inlay = "#eaeaea",
 }
