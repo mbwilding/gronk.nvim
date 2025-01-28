8c720675fb6558599f2ae7e7566ba44679a0ae7c
Matthew Wilding <mbwilding@gmail.com>
2025-01-29 00:40:44 +0800

    2025-01-29-00-40-44

---
+++ lua/gronk/colors/mono.lua
@@ -0,0 +1,36 @@
+return {
+    -- Default
+    fg = "#bdbdbd",
+    bg = "#000000",
+    window_bg = "#101010",
+    window_accent = "#202020",
+
+    -- Code
+    attribute = "#D3D3D3", -- Light gray
+    comment = "#404040",   -- Dark gray
+    constant = "#808080",  -- Gray
+    delimiter = "#696969", -- Dim gray
+    enum = "#BEBEBE",      -- Gainsboro
+    escape = "#B0C4DE",    -- Light Steel Blue (for subtle difference)
+    interface = "#DCDCDC", -- Gainsboro
+    keyword = "#A8A8A8",   -- Custom Dark gray
+    macro = "#C0C0C0",     -- Silver
+    member = "#B0B0B0",    -- Custom Light gray
+    method = "#909090",    -- Darker Gray
+    module = "#D8D8D8",    -- Light gray
+    namespace = "#C8C8C8", -- Custom Silver
+    number = "#AAAAAA",    -- Custom Medium gray
+    operator = "#E0E0E0",  -- Lighter Gray
+    redundant = "#787878", -- Iconic Silver
+    string = "#A7A7A7",    -- Between Dark and Medium gray
+    struct = "#CACACA",    -- Custom Light gray
+    variable = "#9E9E9E",  -- Medium-Dark Gray
+
+    -- Other
+    ok = "#B2B2B2",    -- Custom Medium-Light gray
+    info = "#BEBEBE",  -- Gainsboro
+    hint = "#CDCDCD",  -- Light gray
+    warn = "#8B8B8B",  -- Dark Gray
+    error = "#5C5C5C", -- Custom Dim gray
+    inlay = "#EAEAEA", -- Very Light gray
+}
