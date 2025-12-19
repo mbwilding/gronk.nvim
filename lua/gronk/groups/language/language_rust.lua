feb79d7efd8fb88f21689c812624ffa535f48ef2
Matthew Wilding <mbwilding@gmail.com>
2025-12-20 00:25:30 +0800

    update

--- lua/gronk/groups/language/language_rust.lua
+++ lua/gronk/groups/language/language_rust.lua
@@ -5,7 +5,9 @@ function M.get(c)
         ["@lsp.mod.crateRoot"] = { fg = c.namespace },
         ["@lsp.mod.declaration.rust"] = { fg = c.member },
         ["@lsp.typemod.namespace.crateRoot"] = { fg = c.namespace },
+        ["@lsp.typemod.variable.declaration.rust"] = { fg = c.struct },
         ["@type.qualifier.rust"] = { fg = c.keyword, underline = true },
+        ["rustModPath"] = { fg = c.namespace },
     }
 end
 
