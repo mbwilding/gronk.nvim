97eace819cce8d16e79ac56abe5cc0eb95ab9a98
Matthew Wilding <mbwilding@gmail.com>
2025-12-20 00:04:11 +0800

    update

--- lua/gronk/groups/language/language_csharp.lua
+++ lua/gronk/groups/language/language_csharp.lua
@@ -2,6 +2,7 @@ local M = {}
 
 function M.get(c)
     return {
+        ["@lsp.type.extensionMethod.cs"] = { fg = c.method },
         ["@lsp.type.stringEscapeCharacter.cs"] = { fg = c.escape },
     }
 end
