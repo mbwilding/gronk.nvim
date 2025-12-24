c49ab71c26567c9e59bd8ff856dfee1133779031
Matthew Wilding <mbwilding@gmail.com>
2025-12-24 11:37:04 +0800

    image

--- lua/gronk/groups/language/language_rust.lua
+++ lua/gronk/groups/language/language_rust.lua
@@ -6,6 +6,7 @@ function M.get(c)
         ["@lsp.mod.declaration.rust"] = { fg = c.member },
         ["@lsp.typemod.const.constant.rust"] = { fg = c.constant },
         ["@lsp.typemod.namespace.crateRoot"] = { fg = c.namespace },
+        ["@lsp.typemod.parameter.declaration.rust"] = { fg = c.variable },
         ["@lsp.typemod.punctuation.unsafe.rust"] = { fg = c.delimiter },
         ["@type.qualifier.rust"] = { fg = c.keyword, underline = true },
     }
